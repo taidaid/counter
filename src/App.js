d2df32dfba68ddb15a6d79ca012b61f2cf7a4326
Bryan <windsor.bryan@gmail.com>
2019-02-14 18:34:50 -0500

    updated and fixed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./components/Button";
 import "./App.css";
 
-const { useState } = React;
-
 const App = () => {
-	const [count, setCount] = useState(0);
-	const [results, setResult] = useState([]);
-	const incrementCount = increment => {
-		setCount(count + increment);
-	};
-	const reset = () => {
-		setCount(0);
-	};
-	const saveResult = result => {
-		results.push(result);
-		setResult(results);
-	};
+  const [count, setCount] = useState(0);
+  const [results, setResults] = useState([]);
 
-	//Add another button that saves the result of the count to a list of results.
+  const incrementCount = increment => {
+    setCount(count + increment);
+  };
 
-	return (
-		<div className="App">
-			<Button increment={+1} onClickFunction={incrementCount} />
-			<Button increment={+10} onClickFunction={incrementCount} />
-			<Button increment={+100} onClickFunction={incrementCount} />
-			<Button increment={+1000} onClickFunction={incrementCount} />
-			<Button id="result" onClickFunction={reset} />
-			<Button increment={-1} onClickFunction={incrementCount} />
-			<Button increment={-10} onClickFunction={incrementCount} />
-			<Button increment={-100} onClickFunction={incrementCount} />
-			<Button increment={-1000} onClickFunction={incrementCount} />
-			<Button id="save" onClickFunction={() => saveResult(count)} />
-			<div>
-				<strong>
-					<em>Count: </em>
-				</strong>
-				{count}
-			</div>
-			<ul>
-				<div>
-					<strong>Results</strong>
-				</div>
-				{results.map(result => (
-					<li>{result}</li>
-				))}
-			</ul>
-		</div>
-	);
+  const reset = () => {
+    setCount(0);
+  };
+
+  const saveResult = () => {
+    const updatedResults = [...(results || []), count];
+    console.log(updatedResults);
+
+    setResults(updatedResults);
+  };
+
+  //Add another button that saves the result of the count to a list of results.
+
+  return (
+    <div className="App">
+      <Button increment={+1} onClickFunction={incrementCount} />
+      <Button increment={+10} onClickFunction={incrementCount} />
+      <Button increment={+100} onClickFunction={incrementCount} />
+      <Button increment={+1000} onClickFunction={incrementCount} />
+      <Button id="result" onClickFunction={reset} />
+      <Button increment={-1} onClickFunction={incrementCount} />
+      <Button increment={-10} onClickFunction={incrementCount} />
+      <Button increment={-100} onClickFunction={incrementCount} />
+      <Button increment={-1000} onClickFunction={incrementCount} />
+      <Button id="save" onClickFunction={saveResult} />
+      <div>
+        <strong>
+          <em>Count: </em>
+        </strong>
+        {count}
+      </div>
+      <ul>
+        <div>
+          <strong>Results</strong>
+        </div>
+        {console.log(results)}
+        {results.map((result, index) =>
+          result ? <li key={index}>{result}</li> : null
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 const Button = ({ increment, onClickFunction, id }) => {
-	const handleClick = () => {
-		onClickFunction(increment);
-	};
-	if (increment < 0) {
-		return <button onClick={handleClick}>{increment}</button>;
-	} else if (increment > 0) {
-		return <button onClick={handleClick}>+{increment}</button>;
-	} else if (id === "result") {
-		return <button onClick={handleClick}>Reset</button>;
-	} else {
-		return <button onClick={handleClick}>Save</button>;
-	}
+  const handleClick = () => {
+    onClickFunction(increment);
+  };
+
+  if (increment < 0) {
+    return <button onClick={handleClick}>{increment}</button>;
+  } else if (increment > 0) {
+    return <button onClick={handleClick}>+{increment}</button>;
+  } else if (id === "result") {
+    return <button onClick={onClickFunction}>Reset</button>;
+  } else {
+    return <button onClick={onClickFunction}>Save</button>;
+  }
 };
 
 export default Button;
